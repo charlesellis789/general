4dd8a9a3a026d2648e474f7b989218a10381d432
Charles Ellis <charlesellis789@gmail.com>
2022-05-13 23:18:58 -0400

    Generalized code, removed repetitions, added error handling

--- database.py
+++ database.py
@@ -1,47 +1,26 @@
 import psycopg2
-
-# Checks if a database exists. If not, it calls a function to create the database
-# Parameters: DBName (string) = Name of the database to check
-# Returns nothing
-def DBExists(Host,DBName,UserName,Password):
-	try: # Attempts to connect to the database
-		conn = psycopg2.connect(
-			host=Host,
-			database=DBName,
-			user=UserName,
-			password=Password)
-		conn.close()
-		#return(True)
-		return True
-	except Exception as e: # If database connection fails, error is caught, user is notified.
-		#return (False,e)
-		return False
-
-def dropDB(Host,DBName,UserName,Password):
-	conn = psycopg2.connect(
-			host=Host,
-			database=DBName,
-			user=UserName,
-			password=Password)
-	conn.autocommit = True
-	cursor = conn.cursor()
+	
+# Connects to a database
+# Parameters: DBName (string) = name of the database connecting to
+# Returns the cursor and connection to run queries
+def connectToDB(Host,DBName,UserName,Password):
 	try:
-		cursor.execute('drop database if exists ' + DBName + ';')
-		return True
+		conn = psycopg2.connect(
+		    host=Host,
+		    database=DBName,
+		    user=UserName,
+		    password=Password)
+		conn.autocommit = True
+		return conn
 	except:
 		return False
+#	cursor = conn.cursor()
+#	return [cursor,conn]
 
 # Creates a new database using the postgres database that already exists. Then adds all starter tables / values to the new database
 # Parameters: newDBName (String) = Name of the database being created
 # Returns nothing
 def createDB(conn,newDBName):
-#	conn = psycopg2.connect(
-#			host="localhost",
-#			database="postgres",
-#			user="postgres",
-#			password=var.databasePassword)
-	conn.autocommit = True
-	cursor = conn.cursor()
 	sql = '''
 		CREATE DATABASE ''' + newDBName + '''
 		WITH 
@@ -49,61 +28,42 @@ def createDB(conn,newDBName):
 		ENCODING = 'UTF8'
 		CONNECTION LIMIT = -1;'''
 	try:
+		cursor = conn.cursor()
 		cursor.execute(sql)
-		conn.close()
 		return True
 	except:
 		return False
-	
-# Connects to a database
-# Parameters: DBName (string) = name of the database connecting to
-# Returns the cursor and connection to run queries
-def connectToDB(DBName):
-	conn = psycopg2.connect(
-	    host="localhost",
-	    database=DBName,
-	    user="postgres",
-	    password=var.databasePassword)
-	cursor = conn.cursor()
-	return [cursor,conn]
 
-# Closes database connection
-# Parameters:
-# - cursor (cursor object)
-# - conn (connection details)
-# returns nothing
-def closeDBConnection(cursor,conn):
-	cursor.close()
-	conn.close()
+def dropDB(conn,DBName):
+	try:
+		cursor = conn.cursor()
+		cursor.execute('drop database if exists ' + DBName + ';')
+		return True
+	except:
+		return False
 
 # Runs sql that changes the database aka insert, delete, update queries
 # Parameters:
 # - sql (string) = sql to run
 # - DBName (string) = Name of the database to run the sql on
 # Returns nothing
-def runSql(sql,DBName):
-	[cursor,conn] = connectToDB(DBName)
-	cursor.execute(sql)
-	conn.commit()
-	closeDBConnection(cursor,conn)
+def executeSql(conn,sql):
+	try:
+		cursor = conn.cursor()
+		cursor.execute(sql)
+		conn.commit()
+		return True
+	except Exception as e:
+		return False
 
-def runSqlAutocommit(sql,DBName):
-	conn = psycopg2.connect(
-	    host="localhost",
-	    database=DBName,
-	    user="postgres",
-	    password=var.databasePassword)
-	conn.autocommit = True
-	cursor = conn.cursor()
-	cursor.execute(sql)
-	closeDBConnection(cursor,conn)
-
-def runSelectQuery(sql):
-	[cursor,conn] = connectToDB(var.databaseName)
-	cursor.execute(sql)
-	data = cursor.fetchall()
-	closeDBConnection(cursor,conn)
-	return data
+def runSelectQuery(conn,sql):
+	try:
+		cursor = conn.cursor()
+		cursor.execute(sql)
+		data = cursor.fetchall()
+		return data
+	except:
+		return False
 
 # Creates a temporary table for more advanced data insertion
 # Parameters:
@@ -111,12 +71,18 @@ def runSelectQuery(sql):
 # - columns (string) = all columns necessary in the temp table. *Note: variable type definitions can only be the ones listed below. Please add if necessary
 # Returns: sql (string) = all sql necessary to create a new temp table and insert data. Does not include the data.
 def createTempTable(tempTableName,columns):
-	sql = """\ndrop table if exists """ + tempTableName + """;\n
-	create temporary table """ + tempTableName + """ (
-	""" + columns + """
-	);
-	insert into """ + tempTableName + """ (
-	""" + columns.replace(' decimal','').replace(' varchar(50)','').replace(' date','').replace(' int','').replace(' boolean','') + """ 
-	) values
-	"""
-	return sql
+	try:
+		tempTableName = str(tempTableName)
+		sql = """\ndrop table if exists """ + tempTableName + """;\ncreate temporary table """ + tempTableName + """ (\n"""
+		sqlInsert = 'insert into ' + tempTableName + ' (\n'
+		sqlCreateColumns = ''
+		sqlInsertColumns = ''
+		for x in columns:
+			sqlCreateColumns += '\t' + str(x[0]) + ' ' + str(x[1]) + ',\n'
+			sqlInsertColumns += '\t' + str(x[0]) + ',\n'
+		sqlCreateColumns = sqlCreateColumns[:-2] + '\n);\n'
+		sqlInsertColumns = sqlInsertColumns[:-2] + '\n)\nvalues\n'
+		sql += sqlCreateColumns + sqlInsert + sqlInsertColumns
+		return sql
+	except:
+		return False
