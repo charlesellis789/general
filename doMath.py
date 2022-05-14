341bb439079cfae63c798055a2a207f76fe59e77
Charles Ellis <charlesellis789@gmail.com>
2022-05-14 11:54:00 -0400

    updated regressions

--- doMath.py
+++ doMath.py
@@ -4,25 +4,24 @@ import numpy as np
 def toOrdinal(n): # Returns a string variable that was a parameter n (int) converted to an ordinal value. (1 => 1st, 2 => 2nd, etc)
 	return str(n) + {1: 'st', 2: 'nd', 3: 'rd'}.get(4 if 10 <= n % 100 < 20 else n % 10, "th")
 
-def performRegression(y_points,degrees):
-	x_points = list(range(0,len(y_points)))
-	coef = poly.polyfit(x_points,y_points,degrees)
+def performRegression(xValues,yValues,degreesOfRegression):
+	coef = poly.polyfit(xValues,yValues,degreesOfRegression)
 	residual = 0
 	sumOfSquares = 0
-	mean = sum(y_points) / len(y_points)
-	for i in range(0,len(x_points)):
+	mean = sum(yValues) / len(yValues)
+	for i in range(0,len(xValues)):
 		y = 0
-		for j in range(0,degrees+1):
-			y = y + coef[j]*x_points[i]**j
-		residual = residual + (y-y_points[i])**2
-		sumOfSquares = sumOfSquares + (mean-y_points[i])**2
+		for j in range(0,degreesOfRegression+1):
+			y = y + coef[j]*xValues[i]**j
+		residual = residual + (y-yValues[i])**2
+		sumOfSquares = sumOfSquares + (mean-yValues[i])**2
 	if sumOfSquares != 0:
 		r_squared = 1 - residual / sumOfSquares
 	else:
-		r_squared = -1
+		r_squared = 1
 	return [np.flip(coef),r_squared]
 
-def interpolate(coefs,x_value):
+def interpolatePolynomialEquation(coefs,x_value):
 	degree = len(coefs)
 	interpolatedValue = 0
 	for x in coefs:
